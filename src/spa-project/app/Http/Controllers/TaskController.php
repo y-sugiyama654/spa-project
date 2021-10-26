71a788b7cf6c34a8e15619fa4cf4ceb802ee3938
Yuta <y.sugiyama54@gmail.com>
2021-10-26 13:01:35 +0900

    Task一覧取得APIの実装

--- src/spa-project/app/Http/Controllers/TaskController.php
+++ src/spa-project/app/Http/Controllers/TaskController.php
@@ -10,21 +10,11 @@ class TaskController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Task[]|\Illuminate\Database\Eloquent\Collection
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return Task::all();
     }
 
     /**
@@ -45,17 +35,6 @@ class TaskController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Task $task)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Task $task)
     {
         //
     }
