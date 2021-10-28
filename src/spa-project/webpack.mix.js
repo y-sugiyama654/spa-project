ac9e63e617a1e0417a42658e15575972cfd37693
Yuta <y.sugiyama54@gmail.com>
2021-10-28 23:24:16 +0900

    typescriptとreactのインストール

--- src/spa-project/webpack.mix.js
+++ src/spa-project/webpack.mix.js
@@ -11,7 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.ts('resources/ts/index.tsx', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
